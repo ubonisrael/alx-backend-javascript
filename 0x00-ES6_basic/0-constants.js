cf89ffaf28f73882de282eecb194bad1f90ff20b
ubonisrael <akpanudohubonisrael@gmail.com>
2024-05-27 12:10:35 +0100

    0x00 task 1 - disabled elsint no-unused-vars

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -13,20 +13,3 @@ export function taskNext() {
 
   return combination;
 }
-// function taskFirst() {
-//   const task = 'I prefer const when I can.';
-//   return task;
-// }
-
-// function getLast() {
-//   return ' is okay';
-// }
-
-// function taskNext() {
-//   let combination = 'But sometimes let';
-//   combination += getLast();
-
-//   return combination;
-// }
-
-// module.exports = { taskFirst, taskNext };
