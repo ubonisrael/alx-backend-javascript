094c4f099e22b796eb4bead8ce7574790c4df446
ubonisrael <akpanudohubonisrael@gmail.com>
2024-05-28 23:46:26 +0100

    0x01 task 6 - fixed issues in code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,5 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => data.map((dat) => ({ status: dat.status, value: dat.value || dat.reason })));
 }
