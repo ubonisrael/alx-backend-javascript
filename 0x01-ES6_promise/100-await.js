f80083e8eb88a83f80a4238b88a51796c416eedd
ubonisrael <akpanudohubonisrael@gmail.com>
2024-05-29 00:03:45 +0100

    0x01 task 100 - initial attempt

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,16 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo,
+      user,
+    };
+  } catch (e) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
