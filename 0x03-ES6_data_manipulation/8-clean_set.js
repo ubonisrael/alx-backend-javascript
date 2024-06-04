445c8e616741e959ee906823a97a7bef19c3817c
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-04 12:13:52 +0100

    0x03 task 8 - correction attempt 4

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (
-    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+    !set || !startString || !(set instanceof Set) || typeof startString !== 'string'
   ) {
     return '';
   }
