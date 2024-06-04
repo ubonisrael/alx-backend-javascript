10a43efee6b70f90ba80da913497bf122603d5b8
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-04 12:03:33 +0100

    0x03 task 8 - correction attempt

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,10 +2,11 @@ export default function cleanSet(set, startString) {
   const arr = [];
   const splitPosition = startString.length;
 
-  if (splitPosition === 0) return '';
-  for (const x of set.values()) {
-    if (x.startsWith(startString)) {
-      arr.push(x.slice(splitPosition));
+  if (splitPosition !== 0) {
+    for (const x of set.values()) {
+      if (x.startsWith(startString)) {
+        arr.push(x.slice(splitPosition));
+      }
     }
   }
   return arr.join('-');
