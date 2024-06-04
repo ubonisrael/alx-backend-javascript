155ea3da03ca3812ea3997a62d9aac47c883637a
ubonisrael <akpanudohubonisrael@gmail.com>
2024-06-04 12:24:32 +0100

    0x03 task 10 - correction attempt: checked if mapObject is an instance of Map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,7 @@
 export default function updateUniqueItems(mapObject) {
-  for (const [x, y] of mapObject) {
-    // console.log(x, y);
-    if (y === 1) mapObject.set(x, 100);
+  if (mapObject instanceof Map) {
+    for (const [x, y] of mapObject) {
+      if (y === 1) mapObject.set(x, 100);
+    }
   }
 }
