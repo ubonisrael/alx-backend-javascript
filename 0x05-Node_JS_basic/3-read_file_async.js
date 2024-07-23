d2f5408ebc816fcd9a983882e2db575446543645
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-23 15:16:59 +0100

    0x05 task 3 correction

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,26 +5,27 @@ function countStudents(path) {
     readFile(path, { encoding: 'utf8', flag: 'r' }, (err, data) => {
       if (err) {
         reject(new Error('Cannot load the database'));
-      }
-      const obj = {};
-      let total = 0;
-      for (const row of data.trim().split('\n')) {
-        const rowData = row.split(',');
-        if (rowData[0] !== 'firstname') {
-          if (rowData[3] in obj) {
-            obj[rowData[3]].push(rowData[0]);
-            total += 1;
-          } else {
-            obj[rowData[3]] = [rowData[0]];
-            total += 1;
+      } else {
+        const obj = {};
+        let total = 0;
+        for (const row of data.trim().split('\n')) {
+          const rowData = row.split(',');
+          if (rowData[0] !== 'firstname') {
+            if (rowData[3] in obj) {
+              obj[rowData[3]].push(rowData[0]);
+              total += 1;
+            } else {
+              obj[rowData[3]] = [rowData[0]];
+              total += 1;
+            }
           }
         }
+        console.log(`Number of students: ${total}`);
+        for (const [key, value] of Object.entries(obj)) {
+          console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
+        }
+        resolve(true);
       }
-      console.log(`Number of students: ${total}`);
-      for (const [key, value] of Object.entries(obj)) {
-        console.log(`Number of students in ${key}: ${value.length}. List: ${value.join(', ')}`);
-      }
-      resolve(true);
     });
   });
 }
