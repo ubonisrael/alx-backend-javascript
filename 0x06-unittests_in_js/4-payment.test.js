fb42048902ffecef1dbb492f913797f56f601ea3
ubonisrael <akpanudohubonisrael@gmail.com>
2024-07-25 21:26:01 +0100

    0x06 task 4 - initial commit

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const { expect } = require("chai");
+const Utils = require("./utils");
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require("./3-payment");
+
+describe("sendPaymentRequestToApi", function () {
+  it("sendPaymentRequestToApi(100, 20) logs 10 to the console", function () {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils).calculateNumber.returns(10)
+    sendPaymentRequestToApi(100, 20);
+
+    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledWith(`The total is: ${10}`)).to.be.true;
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    
+    spy.restore();
+    stub.restore();
+  });
+});
